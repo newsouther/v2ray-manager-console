c5c623779016d6284cee1c42e28bf2602c29fbcd
南风 <571691797@qq.com>
2020-05-11 17:57:05 +0800

    修改版权网站

--- src/components/GithubCorner/index.vue
+++ src/components/GithubCorner/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/master-coder-ll/v2ray-web-manager" target="_blank" class="github-corner" aria-label="View source on Github">
+  <a href="www.baidu.com" target="_blank" class="github-corner" aria-label="View source on Github">
     <svg
       width="80"
       height="80"
